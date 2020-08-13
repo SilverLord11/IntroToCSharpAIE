34a5729e037e6047d20139bdbebd2974d57077f8
Logan Holt <WolfGuardLegion11@gmail.com>
2020-08-12 17:36:59 -0700

    Add first files

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("These shall be my finest warriors, these men who give of themselves to me.");
+            Console.Write("In great armour, I shall clad them, and with the mightiest weapons shall they be armed.");
+            Console.Write("They are my bulwark against Terror. They are the defenders of humanity.");
+            Console.Write("They are my Space Marines ... and they shall know no fear.");
+            Console.Write("~The Emperor of Mankind");
+            Console.ReadLine();
+        }
+    }
+}
+// 1. Create a Git repository
+//    - .gitignore
+// 2. Add files to the Git repository
+// 3. Commit those files to the Git repository
+// 4. Add a remote
+// 5. Push to the remote
