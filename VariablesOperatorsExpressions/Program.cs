342d66f99222658ab4855f25bcc3b50149999c32
Logan Holt <WolfGuardLegion11@gmail.com>
2020-08-13 11:38:10 -0700

    Add a project for the Variables Tutorial

---
+++ VariablesOperatorsExpressions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VariablesOperatorsExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("No mercy... NO MERCY! FOR TSUSHIMA!");
+            Console.ReadLine();
+        }
+    }
+}
