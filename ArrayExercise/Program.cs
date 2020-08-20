df793415c3a648f90a03e4c01ce9b9ac5082e6e6
Logan Holt <WolfGuardLegion11@gmail.com>
2020-08-20 12:16:32 -0700

    Array Exercise

---
+++ ArrayExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArrayExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = { 2, 6, 10, 14 };
+            Console.WriteLine(values[2]);
+            Console.WriteLine(++values[0]);
+            Console.WriteLine(++values[0]);
+            int x = 2;
+            Console.WriteLine(values[++x]);
+            Console.WriteLine(values[4]);
+        }
+    }
+}
