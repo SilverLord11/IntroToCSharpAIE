53e06b0989abe2379feb83e85cf0aa847cfb7e46
Logan Holt <WolfGuardLegion11@gmail.com>
2020-08-20 11:28:42 -0700

    Exercise for Classes

---
+++ ClassExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassExercise
+{
+    class vector2 
+    {
+        public float x;
+        public float y;
+    
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
