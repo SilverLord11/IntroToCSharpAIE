32f270e528417e916f27eb472aed2a5fdfaddabe
Logan Holt <WolfGuardLegion11@gmail.com>
2020-08-20 12:20:55 -0700

    Array Exercise (Cont.)

--- ArrayExercise/Program.cs
+++ ArrayExercise/Program.cs
@@ -7,12 +7,12 @@ namespace ArrayExercise
         static void Main(string[] args)
         {
             int[] values = { 2, 6, 10, 14 };
-            Console.WriteLine(values[2]);
-            Console.WriteLine(++values[0]);
-            Console.WriteLine(++values[0]);
+            Console.WriteLine(values[2]); //write the second value: 10
+            Console.WriteLine(++values[0]); //increase the zero value, so 2 -->, 3 and write it
+            Console.WriteLine(++values[0]); //increase the zero value again: 3 --> 4, and write it out
             int x = 2;
-            Console.WriteLine(values[++x]);
-            Console.WriteLine(values[4]);
+            Console.WriteLine(values[++x]); //write the third value, which is 14
+            Console.WriteLine(values[4]); //outside of boundry
         }
     }
 }
